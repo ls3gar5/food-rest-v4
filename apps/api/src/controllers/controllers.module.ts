a315a1f49f15dd0413aa523711af777d4d2acfea
Leonardo Segars <lsegars@teco.com.ar>
2025-10-20 11:19:34 -0300

    add new controller user

--- apps/api/src/controllers/controllers.module.ts
+++ apps/api/src/controllers/controllers.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { HealthController } from './health.controller';
+import { UserController } from './user.controller';
+import { UserService } from '../services/user.service';
 
 @Module({
-  controllers: [HealthController],
+  controllers: [HealthController, UserController],
+  providers: [UserService],
 })
 export class ControllersModule {}
